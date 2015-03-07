e064ba22a895bca05cdf637aff0245b3ed9284aa
Eric-Franklin79 <pickle797900@verizon.net>
2015-03-07 01:28:32 -0500

    new

---
+++ js/main.js
@@ -0,0 +1,28 @@
+window.onload = function() {
+    
+    "use strict";
+    
+    
+    var game = new Phaser.Game( 1280, 640, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update, render: render} );
+    
+    function preload() {
+      
+    }
+    
+   function create(){
+   	  
+   }
+    
+   function update(){
+    	   
+   }
+   
+   
+   
+   
+   
+   
+   function render(){
+   	   
+   }
+};
